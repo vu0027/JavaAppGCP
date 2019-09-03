b3d70476d8bb3c00c3906ca0c80711db744d05cc
Anh Vu <46767310+vu0027@users.noreply.github.com>
2019-09-03 12:37:42 -0500

    Update DemoServlet.java

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -29,7 +29,7 @@ public class DemoServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
     resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"World\"}");
+    resp.getWriter().println("{ \"name\": \"World\" }");
 
         
   }
